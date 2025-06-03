2fdbbdf76a356ab1e5f081496c646fa2f3e1fded
Ian Waldron <ian.waldron@scribawaldron.com>
2025-06-03 11:03:16 -0700

    add view only approach

--- demo/urls.py
+++ demo/urls.py
@@ -1,12 +1,20 @@
 from django.urls import path
-from django.views.generic import TemplateView
+# from django.views.generic import TemplateView
 
+from .views import robots_txt_view
+
+# template-based solution
+# urlpatterns = [
+#     path(
+#         'robots.txt',
+#         TemplateView.as_view(
+#             template_name='demo/robots.txt',
+#             content_type='text/plain'
+#         )
+#     )
+# ]
+
+# view-only solution
 urlpatterns = [
-    path(
-        'robots.txt',
-        TemplateView.as_view(
-            template_name='demo/robots.txt',
-            content_type='text/plain'
-        )
-    )
+    path('robots.txt', robots_txt_view),
 ]
--- demo/views.py
+++ demo/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+# noinspection PyUnusedLocal
+def robots_txt_view(request):
+    txt_lines = [
+        'User-agent: Googlebot',
+        'Disallow: /nogooglebot/',
+        '',
+        'User-agent: *',
+        'Allow: /',
+        '',
+        'Sitemap: https://www.example.com/sitemap.xml'
+    ]
+    content = '\n'.join(txt_lines)
+    return HttpResponse(content, content_type='text/plain')
